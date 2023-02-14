2fb656808230dd44931f07cf73b3b10aa9df45a5
Bussink <b.bussink@amsterdam.nl>
2023-02-14 16:46:58 +0100

    improved readability for api query road_sections.py

--- src/app/api/v1/rvv/road_sections.py
+++ src/app/api/v1/rvv/road_sections.py
@@ -59,96 +59,69 @@ def query_db_road_sections_rvv(
         select json_build_object(
             'type','Feature',
             'properties',json_build_object(
-                'bereikbaarheidStatusCode', bereikbaar_status_code,
-                'id', id
-            ),
-            'geometry', geom::json
-        )
+                'bereikbaarheidStatusCode', level_a.bereikbaar_status_code,
+                'id', level_a.id),
+            'geometry', level_a.geom::json)
         from (
-            select v.id,
-            case
-                when v.bereikbaar_status_code = 111 then 11001
-                when v.bereikbaar_status_code = 222 then 11001
-                when v.bereikbaar_status_code = 333 then 11001
-                when v.bereikbaar_status_code = 444 then 11100
-                else 999
-            end as bereikbaar_status_code,
-            st_transform(v.geom,4326) as geom
+            select level_aa.id,
+            case	when level_aa.bereikbaar_status_code = 111 then 11001
+                    when level_aa.bereikbaar_status_code = 222 then 11001
+                    when level_aa.bereikbaar_status_code = 333 then 11001
+                    when level_aa.bereikbaar_status_code = 444 then 11100
+                    else 999 end as bereikbaar_status_code,
+            st_transform(level_aa.geom,4326) as geom
             from (
-                select
-                    abs(n.id) as id,
-                    max(
-                        case
-                            when n.cost is NULL then 111
-                            when routing.agg_cost is null then 222
-                            when n.c07 is true and %(bedrijfsauto)s is true
-                                and %(max_massa)s > 3500
-                                 or n.c07a is true and %(bus)s is true
-                                 or n.c10 is true and %(aanhanger)s is true
-                                or n.c17 < %(lengte)s
-                                or n.c18 < %(breedte)s
-                                or n.c19 < %(hoogte)s
-                                or n.c20 < %(aslast)s
-                                or n.c21 < %(gewicht)s
-                                then 333
-                            else 999
-                        end
-                    ) as bereikbaar_status_code,
-                    g.geom
-                from bereikbaarheid.nwb_wegvakken_selection_enriched_directed n
+                select abs(level_aaa.id) as id,
+                max(
+                case	when level_aaa.cost is NULL then 111
+                        when level_aab.agg_cost is null then 222
+                        when level_aaa.c07 is true and %(bedrijfsauto)s is true
+                        and %(max_massa)s > 3500
+                        or level_aaa.c07a is true and %(bus)s is true
+                        or level_aaa.c10 is true and %(aanhanger)s is true
+                        or level_aaa.c17 < %(lengte)s
+                        or level_aaa.c18 < %(breedte)s
+                        or level_aaa.c19 < %(hoogte)s
+                        or level_aaa.c20 < %(aslast)s
+                        or level_aaa.c21 < %(gewicht)s
+                        then 333
+                        else 999 end ) as bereikbaar_status_code,
+                level_aac.geom
+                from bereikbaarheid.nwb_wegvakken_selection_enriched_directed level_aaa
                 left join (
                     SELECT start_vid as source,
                     end_vid as target,
-                    agg_cost FROM pgr_dijkstraCost('
+                    agg_cost
+                    FROM pgr_dijkstraCost('
                         select id, source, target, cost
-                        from bereikbaarheid.nwb_wegvakken_selection_enriched_directed
-                        where cost > 0
-                        and (
-                            (( -.01 + %(lengte)s ) < c17 or c17 is null)
-                            and (( -.01 + %(breedte)s ) < c18 or c18 is null)
-                            and (( -.01 +%(hoogte)s ) < c19 or c19 is null)
-                            and (( -1 + %(aslast)s ) < c20 or c20 is null)
-                            and (( -1 + %(gewicht)s ) < c21 or c21 is null)
-                            and (
-                                c07 is false
-                                or (c07 is true and %(bedrijfsauto)s is false)
-                                or (
-                                    c07 is true
-                                    and %(bedrijfsauto)s is true
-                                    and %(max_massa)s <= 3500
-                                )
-                            )
-                             and (
-                                 c07a is false
-                                 or (c07a is true and %(bus)s is false)
-                             )
-                             and (
-                                 c10 is false
-                                 or (c10 is true and %(aanhanger)s is false)
-                             )
-                        )',
+                        from bereikbaarheid.nwb_wegvakken_selection_enriched_directed as level_aaba
+                        where level_aaba.cost > 0
+                        and ((( -.01 + %(lengte)s ) < level_aaba.c17 or level_aaba.c17 is null)
+                            and (( -.01 + %(breedte)s ) < level_aaba.c18 or level_aaba.c18 is null)
+                            and (( -.01 +%(hoogte)s ) < level_aaba.c19 or level_aaba.c19 is null)
+                            and (( -1 + %(aslast)s ) < level_aaba.c20 or level_aaba.c20 is null)
+                            and (( -1 + %(gewicht)s ) < level_aaba.c21 or level_aaba.c21 is null)
+                            and 	( level_aaba.c07 is false or (level_aaba.c07 is true and %(bedrijfsauto)s is false)
+                                    or (level_aaba.c07 is true and %(bedrijfsauto)s is true and %(max_massa)s <= 3500))
+                            and (level_aaba.c07a is false or (level_aaba.c07a is true and %(bus)s is false))
+                            and (level_aaba.c10 is false or (level_aaba.c10 is true and %(aanhanger)s is false))
+                            )',
                         271319101,
-                        array(
-                            select node
-                            from bereikbaarheid.nwb_wegvakken_selection_node
-                        )
-                    )
-                ) as routing on n.source = routing.target
-                left join bereikbaarheid.nwb_wegvakken_selection_enriched_directed g
-                    on abs(n.id) = g.id
-                    where abs(n.id) in (
-                        select id
-                        from bereikbaarheid.nwb_wegvakken_selection_enriched_directed
-                        where id > 0
-                    )
-                    and n.cost > 0
-                    and n.gme_id = 344
-                    and n.wegbehsrt = 'G'
-                group by abs(n.id), g.geom
-                order by abs(n.id)
-            ) v
-            where v.bereikbaar_status_code <> 999
-        ) m """  # noqa: E501 - ignore line-length in case of long table names
+                        array(select level_aabx.node from bereikbaarheid.nwb_wegvakken_selection_node as level_aabx))
+                ) as level_aab
+                on level_aaa.source = level_aab.target
+                left join bereikbaarheid.nwb_wegvakken_selection_enriched_directed level_aac
+                on abs(level_aaa.id) = level_aac.id
+                where abs(level_aaa.id) in (select level_aax.id from bereikbaarheid.nwb_wegvakken_selection_enriched_directed as level_aax where level_aax.id > 0)
+                and level_aaa.cost > 0
+                and level_aaa.gme_id = 344
+                and level_aaa.wegbehsrt = 'G'
+                group by abs(level_aaa.id), level_aac.geom
+                order by abs(level_aaa.id)
+            ) as level_aa
+            where level_aa.bereikbaar_status_code <> 999
+        ) as level_a
+        """  # noqa: E501 - ignore line-length in case of long table names
 
     query_params = {
         "bus": vehicleTypes.vehicle_is_bus(vehicle_type),
